472dcc9528c7a927c140453e9fbd146bce0cb18a
Blackice-x <122108432+blackice-x@users.noreply.github.com>
2025-04-16 21:40:40 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,16 @@
+const { Client, GatewayIntentBits, Partials } = require("discord.js");
+const client = new Client({
+  intents: Object.values(GatewayIntentBits), 
+  partials: Object.values(Partials),
+  shards: "auto"
+});
+const config = require("./src/config.js");
+const { readdirSync } = require("node:fs");
+
+let token = config.token;
+readdirSync("./src/utils").map(async (file) => {
+const util = await require(`./src/utils/${file}`);
+util.execute(client);
+});
+const color = '#14c9d3';
+client.login(token);
